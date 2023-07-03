82f947213506548f782b4e6d40645a2bbc6a875f
GamingOnInsulin <59486309+gamingoninsulin@users.noreply.github.com>
2023-07-03 23:55:05 +0200

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,6 @@ import sitemap from '@astrojs/sitemap';
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://gamingoninsulin.github.io',
-	base: 'cluster',
+	base: '/npm-cluster',
 	integrations: [mdx(), sitemap()],
 });
